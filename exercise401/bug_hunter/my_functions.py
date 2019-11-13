758fa90b9509f12a0a9b85cab9231f241a220d3a
Thibaud Colas <thibaudcolas@gmail.com>
2019-11-13 20:33:24 +0000

    Add solutions for remaining exercises

--- exercise401/bug_hunter/my_functions.py
+++ exercise401/bug_hunter/my_functions.py
@@ -5,5 +5,6 @@ def boxes_required(qty, box_capacity=6):
     :param box_capacity: How many eggs will fit in a box
     :return: The number of boxes that are required
     """
-    return int(qty / box_capacity) + 1
-    # return qty % box_capacity + 1
+    reminder = qty % box_capacity
+    extra = 0 if reminder == 0 else 1
+    return int((qty - reminder) / box_capacity) + extra
--- exercise401/tests/test_bug_hunter.py
+++ exercise401/tests/test_bug_hunter.py
@@ -3,21 +3,17 @@ from collections import namedtuple
 
 from bug_hunter import my_functions
 
-TestData = namedtuple('TestData', ['qty', 'result'])
+TestData = namedtuple("TestData", ["qty", "result"])
 
 
 class Test401(unittest.TestCase):
     def test_boxes_required(self):
-        data = [TestData(1, 1),
-                TestData(11, 2),
-                TestData(12, 2),
-                TestData(17, 3)]
+        data = [TestData(1, 1), TestData(11, 2), TestData(12, 2), TestData(17, 3)]
 
         for test in data:
             with self.subTest(value=test.qty):
-                self.assertEqual(test.result,
-                                 my_functions.boxes_required(test.qty))
+                self.assertEqual(test.result, my_functions.boxes_required(test.qty))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main(verbosity=2)
